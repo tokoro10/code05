9b75eb9d315caf9f2f1ce5064cff4985e58cd614
tokoro10 <hijoukin98@yahoo.co.jp>
2016-05-12 12:22:18 +0900

    第五回練習問題７

--- js/number.js
+++ js/number.js
@@ -3,11 +3,19 @@ function isEvenNumber(number){
     
 }
 
-function isAcceptableNumber(){
+function isAcceptableNumber(numer){
+    return (number % 2 != 0) && (number % 3 != 0);
 }
 
-function isMultipleOf5(){
+function isMultipleOf5(number){
+    if (number % 5 == 0){
+        return 1;
+    } else {
+        return 0;
+    }
+    
 }
 
 function divideBy2(){
+    
 }
